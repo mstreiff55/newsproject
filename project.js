767c3bc692e73f16065b080a55426a9a675fd2f7
mstreiff55 <mstreiff55@gmail.com>
2017-05-17 17:26:48 -0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,4 @@
+var newsapi = new XMLHttpRequest();
+newsapi.open("GET", "https://newsapi.org/v1/articles?source=the-wall-street-journal&sortBy=top&apiKey=1b611f6afdb8436293764698c56200f7")
+console.log(newsapi)
+/* 1b611f6afdb8436293764698c56200f7
